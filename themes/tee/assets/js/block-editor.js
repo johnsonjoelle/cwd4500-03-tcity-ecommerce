d14d35cbb11a4e3a77c42ff62415c277b572dc01
Joelle Johnson <joelle.johnson@dcmail.ca>
2022-02-12 11:24:42 -0500

    Block Styling up to Video Block (in order of all-blocks page)

--- themes/tee/assets/js/block-editor.js
+++ themes/tee/assets/js/block-editor.js
@@ -1,14 +1,37 @@
 // * Quote Blocks
-// Add style
-wp.blocks.registerBlockStyle( 'core/quote', {
-    name: 'fancy-quote',
-    label: 'Fancy Quote',
-} );
-
-// Unregister Style
 wp.domReady( function () {
     wp.blocks.unregisterBlockStyle( 'core/quote', 'plain' );
 } );
+
+// * Table Blocks
+wp.blocks.registerBlockStyle( 'core/table', {
+    name: 'redheader-table',
+    label: 'Red Header',
+} );
+wp.blocks.registerBlockStyle( 'core/table', {
+    name: 'blueheader-table',
+    label: 'Blue Header',
+} );
+wp.domReady( function () {
+    wp.blocks.unregisterBlockStyle( 'core/table', 'stripes' );
+} );
+
+// * Image Blocks
+wp.domReady( function () {
+    wp.blocks.unregisterBlockStyle( 'core/image', 'rounded' );
+} );
+
+// * Button Blocks
 wp.domReady( function () {
     wp.blocks.unregisterBlockStyle( 'core/button', 'outline' );
+} );
+
+// Column Blocks
+wp.blocks.registerBlockStyle( 'core/column', {
+    name: 'blue-column',
+    label: 'Blue Border Left',
+} );
+wp.blocks.registerBlockStyle( 'core/column', {
+    name: 'red-column',
+    label: 'Red Border Left',
 } );
