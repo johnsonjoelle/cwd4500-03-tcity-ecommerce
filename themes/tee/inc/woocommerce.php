e02f76322857c9b3b9115dcc8288fdced38a6a59
Joelle Johnson <joelle.johnson@dcmail.ca>
2022-03-05 16:21:09 -0500

    Overwrote default variable.php in woocommerce
    Added a copy of variable.php to theme folder. Removed the table format of the variables so could do custom styling.

--- themes/tee/inc/woocommerce.php
+++ themes/tee/inc/woocommerce.php
@@ -15,4 +15,6 @@ function tee_use_block_editor_for_post_type( $use_block_editor, $post_type ) {
 add_filter( 'use_block_editor_for_post_type', 'tee_use_block_editor_for_post_type', 10, 2 );
 
 // Disable the default styles
-add_filter( 'woocommerce_enqueue_styles', '__return_false' );
+// add_filter( 'woocommerce_enqueue_styles', '__return_false' );
+// Add Product Title
+add_action( 'woocommerce_before_single_product_summary', 'woocommerce_template_single_title', 4 );
