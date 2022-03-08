1347dc136a93729590184b1811bf7c8e10a1e880
Joelle Johnson <joelle.johnson@dcmail.ca>
2022-03-08 13:21:46 -0500

    File upload option added to single product page
    file upload done through wp-file-upload plugin

--- themes/tee/inc/woocommerce.php
+++ themes/tee/inc/woocommerce.php
@@ -20,12 +20,8 @@ add_filter( 'woocommerce_enqueue_styles', '__return_false' );
 add_action( 'woocommerce_before_single_product_summary', 'woocommerce_template_single_title', 4 );
 // Add File Sumbission option
 function tee_template_submit_file() {
-    echo '
-    <p class="product-upload-header">Upload my own design</p>
-    <div class="wp-block-file">
-        <a id="wp-block-file--media-8a1d967b-7224-45a2-b71e-03aec0800ec3" href="http://tcity.local/wp-content/uploads/2022/02/product-ideas.txt">Select a file</a>
-        <a href="http://tcity.local/wp-content/uploads/2022/02/product-ideas.txt" class="wp-block-file__button" download="" aria-describedby="wp-block-file--media-8a1d967b-7224-45a2-b71e-03aec0800ec3">Download</a>
-    </div>';
+    echo '<p class="product-upload-header">Upload my own design</p>';
+    echo do_shortcode('[wordpress_file_upload]');
 }
 add_action( 'woocommerce_single_variation', 'tee_template_submit_file', 4 );
 // Move Payment Info on Checkout page
