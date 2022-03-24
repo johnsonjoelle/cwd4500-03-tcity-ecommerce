fa779ad1d2cb29e3b4a7a4a06f236c21c09402d3
Joelle Johnson <joelle.johnson@dcmail.ca>
2022-03-23 21:20:47 -0400

    Updated cart-replacer.js to run after page loads for better consistency

--- themes/tee/assets/js/cart-replacer.js
+++ themes/tee/assets/js/cart-replacer.js
@@ -1,16 +1,18 @@
-let variations = document.querySelectorAll('dt');
+window.onload = function() {
+    let variations = document.querySelectorAll('dt');
 
-for (let variation of variations) {
-    let text = variation.innerText;
-    let newText;
-    if (text=="Choose a colour for your T::") {
-        newText = "Colour:";
-    } else if (text=="Choose a colour for your design::") {
-        newText = "Design Colour:";
-    } else if (text=="Choose a design:") {
-        newText = "Design:";
-    } else {
-        newText = text.replace("::", ":");
+    for (let variation of variations) {
+        let text = variation.innerText;
+        let newText;
+        if (text=="Choose a colour for your T::") {
+            newText = "Colour:";
+        } else if (text=="Choose a colour for your design::") {
+            newText = "Design Colour:";
+        } else if (text=="Choose a design:") {
+            newText = "Design:";
+        } else {
+            newText = text.replace("::", ":");
+        }
+        variation.innerText = newText;
     }
-    variation.innerText = newText;
 }
