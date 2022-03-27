1096d75909f78f2728d33c3a56b8ee474f17b7e6
Joelle Johnson <joelle.johnson@dcmail.ca>
2022-03-27 14:15:28 -0400

    Design post type Archive Page styled

--- themes/tee/archive-tee_design.php
+++ themes/tee/archive-tee_design.php
@@ -14,29 +14,33 @@ get_header();
 
 		<?php if ( have_posts() ) : ?>
 
-			<header class="page-header">
+			<header class="page-header entry-header">
 				<?php
 				the_archive_title( '<h1 class="page-title">', '</h1>' );
-				the_archive_description( '<div class="archive-description">', '</div>' );
 				?>
 			</header><!-- .page-header -->
 
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
+			<section class="entry-content">
+				<div class="tee-posts-list tee-designs-list">
+					<?php
+					/* Start the Loop */
+					while ( have_posts() ) :
+						the_post();
 
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
+						/*
+						* Include the Post-Type-specific template for the content.
+						* If you want to override this in a child theme, then include a file
+						* called content-___.php (where ___ is the Post Type name) and that will be used instead.
+						*/
+						get_template_part( 'template-parts/content', 'excerpt' );
 
-			endwhile;
+					endwhile;
 
-			the_posts_navigation();
-
+					the_posts_navigation();
+					?>
+				</div>
+			</section>
+		<?php
 		else :
 
 			get_template_part( 'template-parts/content', 'none' );
