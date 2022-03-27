3a48cf168e3a1bf74e9a59945720c9e71617a99c
Joelle Johnson <joelle.johnson@dcmail.ca>
2022-03-27 14:42:45 -0400

    Minor Edits to archive.php and style changes to archive pages
    Changes to archive pages to match rest of theme.

--- themes/tee/archive.php
+++ themes/tee/archive.php
@@ -14,27 +14,32 @@ get_header();
 
 		<?php if ( have_posts() ) : ?>
 
-			<header class="page-header">
+			<header class="page-header entry-header">
 				<?php
 				the_archive_title( '<h1 class="page-title">', '</h1>' );
-				the_archive_description( '<div class="archive-description">', '</div>' );
 				?>
 			</header><!-- .page-header -->
 
+			<section class="entry-content">
+				<div class="tee-posts-list">
+					<?php
+					/* Start the Loop */
+					while ( have_posts() ) :
+						the_post();
+
+						/*
+						* Include the Post-Type-specific template for the content.
+						* If you want to override this in a child theme, then include a file
+						* called content-___.php (where ___ is the Post Type name) and that will be used instead.
+						*/
+						get_template_part( 'template-parts/content', 'excerpt' );
+
+					endwhile;
+					?>
+				</div>
+			</section>
+			
 			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
-
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
-
-			endwhile;
-
 			the_posts_navigation();
 
 		else :
